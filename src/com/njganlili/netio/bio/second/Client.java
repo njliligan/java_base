f904f27fd6c09f604253e6a8efe985363724329a
llgan <njganlili@outlook.com>
2022-02-08 21:30:32 +0800

    fix Server.java接收到多个字符，猜测可能是in缓冲区的问题，先match匹配，查资料。

--- src/com/njganlili/netio/bio/second/Client.java
+++ src/com/njganlili/netio/bio/second/Client.java
@@ -13,12 +13,23 @@ public class Client {
         byte[] bytes = new byte[1024];
         BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(client.getOutputStream());
         BufferedInputStream bufferedInputStream = new BufferedInputStream(client.getInputStream());
+        int a = 1;
         while (true) {
+            byte[] buffer = new byte[1024];
+//            int l = bufferedInputStream.read(buffer);
+//            String s = new String(buffer, 0, l);
+//            System.out.printf("received message: %s\n", s);
             bufferedOutputStream.write("hello".getBytes(StandardCharsets.UTF_8), 0, "hello".getBytes(StandardCharsets.UTF_8).length);
             bufferedOutputStream.flush();
-//            int r = -1;
-//            while ((r = bufferedInputStream.read())!= -1) {
-//                System.out.println(Byte.valueOf((byte) r));
+            if(a == 2){
+                bufferedOutputStream.write("close".getBytes(StandardCharsets.UTF_8), 0, "hello".getBytes(StandardCharsets.UTF_8).length);
+                bufferedOutputStream.flush();
+            }
+            a++;
+//            a++;
+//            if (a == 10) {
+//                bufferedOutputStream.write("close".getBytes(StandardCharsets.UTF_8), 0, "close".getBytes(StandardCharsets.UTF_8).length);
+//                bufferedOutputStream.flush();
 //            }
             System.out.println();
             Thread.sleep(1000);
--- src/com/njganlili/netio/bio/second/Server.java
+++ src/com/njganlili/netio/bio/second/Server.java
@@ -48,6 +48,7 @@ public class Server {
                 BufferedInputStream in = new BufferedInputStream(socket.getInputStream());
                 BufferedOutputStream out = new BufferedOutputStream(socket.getOutputStream());
                 for (; ; ) {
+                    System.out.println(Thread.currentThread().getName());
                     System.out.println("reading message.....");
                     byte[] buffer = new byte[1024];
                     // 先读出来, 这里的处理方式肯定不对, 不过只输入 ascii 字符也无所谓
@@ -56,25 +57,29 @@ public class Server {
                     System.out.printf("received message: %s\n", s);
                     // 输入 close 关闭链接
                     // 复制粘贴过去试试
-                    if ("close".equals(s)) {
+                    if (s.contains("close")) {
                         out.write("bye~".getBytes(StandardCharsets.UTF_8));
+                        out.flush();
                         out.close();
+                        in.close();
                         break;
                     }
                     System.out.println(Thread.currentThread().getName()+"writing message.....");
                     System.out.println("writing message.....");
                     //out.write("\r\necho: ".getBytes(StandardCharsets.UTF_8));
-                    out.write("\r\n".getBytes(StandardCharsets.UTF_8));
-                    out.write("内容是:".getBytes(StandardCharsets.UTF_8));
-                    out.write(buffer, 0, l);
+                    //out.write("\r\n".getBytes(StandardCharsets.UTF_8));
+                    //out.write("内容是:".getBytes(StandardCharsets.UTF_8));
+                    //out.write(buffer, 0, l);
                     // 协议: 每个响应消息以 \r\n\r\n 结束
                     //out.write("\r\n\r\n".getBytes(StandardCharsets.UTF_8));
-                    out.write("\r\n".getBytes(StandardCharsets.UTF_8));
-                    out.flush();
+                    //out.write("\r\n".getBytes(StandardCharsets.UTF_8));
+                    //out.flush();
                     System.out.println("finished....");
                     Thread.sleep(3000);
                 }
+                System.out.println("关闭接口");
                 socket.close();
+                System.out.println("socket已关闭");
             } catch (Exception e) {
 
             }
